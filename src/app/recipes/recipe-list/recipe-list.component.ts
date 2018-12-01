645167761f5a41a49a50673b56e556beb641f949
Anthony Ofume <praiseofume@gmail.com>
2018-12-01 16:57:58 +0100

    added bootstrap components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes = [
     { title: "Pancakes", description: "How to make pancakes", author: "Itoro", imageUrl: "https://cdad5c1a4432622c031d-9e2498b30009308de15a0d52a7422974.ssl.cf1.rackcdn.com/menu/product/2-stack-of-pancakes_thumbnaillarge_2017-04-09-19-31-53.jpg" },
-    { title: "Akara", description: "How to make akara", author: "Motunrayo", imageUrl: "" },
-    { title: "Spaghetti", description: "How to make spaghetti", author: "Dayo", imageUrl: "" },
+    { title: "Akara", description: "How to make akara", author: "Motunrayo", imageUrl: "https://cdad5c1a4432622c031d-9e2498b30009308de15a0d52a7422974.ssl.cf1.rackcdn.com/menu/product/2-stack-of-pancakes_thumbnaillarge_2017-04-09-19-31-53.jpg" },
+    { title: "Spaghetti", description: "How to make spaghetti", author: "Dayo", imageUrl: "https://cdad5c1a4432622c031d-9e2498b30009308de15a0d52a7422974.ssl.cf1.rackcdn.com/menu/product/2-stack-of-pancakes_thumbnaillarge_2017-04-09-19-31-53.jpg" },
   ]
   constructor() { }
 
